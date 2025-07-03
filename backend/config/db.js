c281d279666d0076a4f06f14b1ff043bc7ed725f
youssefbennticha <youssefbennticha952@gmail.com>
2025-07-03 20:55:10 +0100

    Fix: changing db connection parametres

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
-console.log("before the try in db.js");
 const db = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "db_bookini",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
 });
 export default db;
